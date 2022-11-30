d15003b60e6bf413ee15b0995d0b004ee87e6e18
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-11-30 11:30:51 -0300

    commit

--- ex014/estilo/scripts.js
+++ ex014/estilo/scripts.js
@@ -3,4 +3,9 @@ var data = new Date()
 var ano = data.getFullYear()
 var fano = document.getElementById("txtano")
 var res = document.querySelector("div#res")
+if (fano.value.length == 0 || fano.value > ano){
+   window.alert("[ERRO] Verifique os dados e tente novamnete")
+}else{
+   window.alert("tudo ok")
+}
 }
