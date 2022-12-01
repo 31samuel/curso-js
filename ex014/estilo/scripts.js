80a623299a9ccf2d38a2fc0f616653ff519d7739
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-01 12:38:18 -0300

    commit

--- ex014/estilo/scripts.js
+++ ex014/estilo/scripts.js
@@ -4,15 +4,16 @@ function verificar(){
     var fano = document.getElementById("txtano")
     var res = document.getElementById("res")
     if (fano.value.length == 0 || fano.value > ano ){
-        window.alert("[ERRO] verifique os dados e tente novamnete!")
+        window.alert("[ERRO] verifique os dados e tente novamente!")
     }else{
-        window.alert("tudo OK")
-        var fsex = document.getElementsByTagName("radsex")
+        var fsex = document.getElementsByName("radsex")
         var idade = ano - Number(fano.value)
-        res.innerHTML ="idade calculada"  +idade +("anos")
-        res.style.padding = "20px"
-        res.style.background = "blue"
-        res.style.color = "#fff"
-        res.style.fontSize = "30px"
-    }
+        var genero =  ""
+      if (fsex[0].checked){
+        genero = "homem"
+      }else if(fsex[1].checked){
+        genero = "mulher"
+      }
+      res.innerHTML = "Detectamos " +genero +idade
+}
 }
