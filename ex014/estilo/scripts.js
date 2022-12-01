34233a1752f5c91e402f0c65884ffd42a96221ad
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-01 13:00:18 -0300

    commit

--- ex014/estilo/scripts.js
+++ ex014/estilo/scripts.js
@@ -9,11 +9,34 @@ function verificar(){
         var fsex = document.getElementsByName("radsex")
         var idade = ano - Number(fano.value)
         var genero =  ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
       if (fsex[0].checked){
         genero = "homem"
+        if(idade >=0 && idade < 10){
+            //criança
+            img.setAttribute("src" , "foto-bebe-m.png")
+        }else if (idade < 21){
+            //jovem
+        }else if (idade < 50){
+            //adulto
+        }else{
+            //idoso
+        }
+
       }else if(fsex[1].checked){
         genero = "mulher"
+        if(idade >=0 && idade < 10){
+            //criança
+        }else if (idade < 21){
+            //jovem
+        }else if (idade < 50){
+            //adulta
+        }else{
+            //idosa
+        }
       }
-      res.innerHTML = "Detectamos " +genero +("com") +idade +("anos")
+      res.innerHTML = "Detectamos " +genero +("com") +idade +("anos") +("👩‍❤️‍💋‍👨")
+      res.appendChild(img)
 }
 }
