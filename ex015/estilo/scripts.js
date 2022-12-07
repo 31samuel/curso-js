b2f06a0db23e7c95f05411161232c457478e7cce
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-07 13:42:31 -0300

    commit

--- ex015/estilo/scripts.js
+++ ex015/estilo/scripts.js
@@ -1,3 +1,4 @@
-function clike(){
-    
-}
+const previousOperationText = document.getElementById("previous-operation");
+const currentOperationText = document.getElementById("current-operation");
+const buttonsOperationText = document.getElementById("buttons-container button");
+
