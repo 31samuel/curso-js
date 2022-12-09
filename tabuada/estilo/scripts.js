a502a11218c3155b2b6fcfd42946e3587dd4df4b
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-09 03:07:19 -0300

    commit

--- tabuada/estilo/scripts.js
+++ tabuada/estilo/scripts.js
@@ -1,17 +1,20 @@
 function tabuada(){
-let num = document.getElementById("txtn")
-let tab = document.getElementById("seltab")
-if (num.Value.length == 0){
-    window.alert("Por favor,digite um numero")
-}else{
-    let n = Number(num.Value)
-    let c = 1
-    while (c <= 10){
-        let item = document.createElement("option")
-        item.text = '${n} x ${c}  = ${n*c}' 
-        tab.appendChild(item)
-        c++
-    }
-}
+   let num = document.getElementById("txtn")
+   let tab = document.getElementById("seltab")
 
+   if(num.value.length == 0){
+    window.alert("por favor,Dgite um numero")
+   }else{
+   let n = Number(num.value)
+   let c = 1
+   tab.innerHTML = ""
+   while (c <= 10){
+    let item  = document.createElement('option')
+    item.text =  +n  +c +n*c 
+    
+    item.value  = "tab" +c
+    tab.appendChild(item)
+    c++
+   }
+   }
 }
