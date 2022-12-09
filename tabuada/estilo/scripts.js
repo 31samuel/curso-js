7e436d71c3c63a7c8d88df95dc35da9619aad46f
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-09 01:53:08 -0300

    commit

---
+++ tabuada/estilo/scripts.js
@@ -0,0 +1,17 @@
+function tabuada(){
+let num = document.getElementById("txtn")
+let tab = document.getElementById("seltab")
+if (num.Value.length == 0){
+    window.alert("Por favor,digite um numero")
+}else{
+    let n = Number(num.Value)
+    let c = 1
+    while (c <= 10){
+        let item = document.createElement("option")
+        item.text = '${n} x ${c}  = ${n*c}' 
+        tab.appendChild(item)
+        c++
+    }
+}
+
+}
